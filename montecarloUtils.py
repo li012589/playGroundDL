9de63d4c53fe10c288242a7afb62e7ffee232b49
Li SH <li012589li@outlook.com>
2017-05-31 14:45:41 +0800

    add adjacent table init func

--- montecarloUtils.py
+++ montecarloUtils.py
@@ -13,11 +13,32 @@ class M:
             self.matrix[self.matrix<0.5] = -1
             self.matrix[self.matrix>=0.5] = 1
     def init(self):
-        pass
+        self.adjacentTab = {}
+        for i in range(self.size):
+            for j in range(self.size):
+                if i-1 < 0:
+                    r = self.size-1
+                else:
+                    r = i-1
+                if i+1 >= self.size:
+                    l = 0
+                else:
+                    l = i+1
+                if j-1 <0:
+                    d = self.size-1
+                else:
+                    d = j-1
+                if j+1 >= self.size:
+                    u = 0
+                else:
+                    u = j+1
+                self.adjacentTab[(i,j)] = [(r,j),(l,j),(i,u),(i,d)]
 
 def main():
     mm = M(4,-0)
+    mm.init()
     print mm.matrix
+    print mm.adjacentTab[(3,3)]
 
 if __name__ == "__main__":
     main()
