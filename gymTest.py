f4a5a1efa794c84d619f70d7910e485fd9074eb7
Li SH <li012589li@outlook.com>
2017-06-05 22:55:38 +0800

    main update

--- gymTest.py
+++ gymTest.py
@@ -1,13 +1,18 @@
 import tensorflow as tf
 import gym
 import numpy as np
+from Qlearn import *
 
 RENDER_ENV = True
-ENV_NAME = 'Pendulum-v0'
+ENV_NAME = 'CartPole-v0'
+BUFFER_SIZE = 10000
+MINIBATCH_SIZE = 64
 
 
 def main():
-    pass
+    env = gym.make(ENV_NAME)
+    sDim = env.observation_space.shape[0]
+    aDim = env.action_space.shape[0]
 
 if __name__ == "__main__":
     main()
