feda844ae6b37de9b1edf476e5be12a1cf5e8d45
Li SH <li012589li@outlook.com>
2017-06-05 20:55:10 +0800

    cartploe Q-learn initial commit

---
+++ Qlearn.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+import numpy as np
+
+class replayBuff:
+    def __init__(self):
+        pass
+
+if __name__ == "__main__":
+    pass
---
+++ gymTest.py
@@ -0,0 +1,13 @@
+import tensorflow as tf
+import gym
+import numpy as np
+
+RENDER_ENV = True
+ENV_NAME = 'Pendulum-v0'
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
