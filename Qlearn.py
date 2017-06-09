59422f2ffc33b300b93a9defc9c596415fe89310
Li SH <li012589li@outlook.com>
2017-06-09 12:38:07 +0800

    debug initTarget func

--- Qlearn.py
+++ Qlearn.py
@@ -5,9 +5,6 @@ import random
 import tflearn
 import gym
 
-USE_RFLEARN = False
-USE_TARGET_NETWORK = True
-
 def weight_variable(shape):
     initial = tf.truncated_normal(shape, stddev = 0.01)
     return tf.Variable(initial)
@@ -59,7 +56,7 @@ class Qnetwork:
         self.targetTrainableVar = tf.trainable_variables()[len(self.trainableVar):]
 
         self.updateTargetNetwork = [self.targetTrainableVar[i].assign(tf.multiply(self.trainableVar[i], self.tau)+tf.multiply(self.targetTrainableVar[i],1-self.tau)) for i in range(len(self.targetTrainableVar))]
-        self.initTargetNetwork = [self.targetTrainableVar[i].assign(self.trainableVar[i])]
+        self.initTargetNetwork = [self.targetTrainableVar[i].assign(self.trainableVar[i]) for i in range(len(self.targetTrainableVar))]
 
         self.a = tf.placeholder(tf.float32, [None, self.aDim])
         self.y = tf.placeholder(tf.float32,[None,1])
@@ -97,77 +94,7 @@ class Qnetwork:
         return self.sess.run(self.initTargetNetwork)
 
 if __name__ == "__main__":
-    sess = tf.InteractiveSession()
-    W_fc1 = weight_variable([4,10])
-    b_fc1 = bias_variable([10])
-    W_fc2 = weight_variable([10,2])
-    b_fc2 = bias_variable([2])
-    #W_fc3 = weight_variable([400, self.aDim])
-    #b_fc3 = bias_variable([self.aDim])
-    s = tf.placeholder(tf.float32, [None,4])
-    h_fc1 = tf.nn.relu(tf.matmul(s, W_fc1) + b_fc1)
-    #h_fc2 = tf.nn.relu(tf.matmul(h_fc1, W_fc2) + b_fc2)
-    #h_fc3 = tf.nn.relu(tf.matmul(h_fc2, W_fc3) + b_fc3)
-    readout = tf.matmul(h_fc1,W_fc2) + b_fc2 #see ddpg for details in init w between -0.003--0.003
-    a = tf.placeholder("float", [None, 2])
-    y = tf.placeholder("float", [None,1])
-    readout_action = tf.reduce_sum(tf.multiply(readout, a), reduction_indices=1)
-    cost = tf.reduce_mean(tf.square(y - readout_action))
-    train_step = tf.train.AdamOptimizer(1e-6).minimize(cost)
-    sess.run(tf.global_variables_initializer())
-
-    ENV_NAME = 'CartPole-v0'
-    env = gym.make(ENV_NAME)
-    s_t = env.reset()
-
-    D = deque()
-    t = 0
-    while True:
-        env.render()
-        if t >= 1000:
-            pass
-            #break
-        t += 1
-        readout_t = readout.eval(feed_dict={s : [s_t]})[0]
-        a_t = np.zeros([2])
-        action_index = 0
-        if random.random() <= 0.001:
-            print("----------Random Action----------")
-            action_index = random.randrange(2)
-            a_t[random.randrange(2)] = 1
-        else:
-            action_index = np.argmax(readout_t)
-            a_t[action_index] = 1
-        s_t1,r_t,terminal,_ = env.step(action_index)
-        D.append((s_t, a_t, r_t, s_t1, terminal))
-        s_t = s_t1
-        if terminal:
-            s_t = env.reset()
-            print("break")
-        if len(D) > 10000:
-            D.popleft()
-        if t > 10:
-            minibatch = random.sample(D, 10)
-            s_j_batch = [d[0] for d in minibatch]
-            a_batch = [d[1] for d in minibatch]
-            r_batch = [d[2] for d in minibatch]
-            s_j1_batch = [d[3] for d in minibatch]
-            y_batch = []
-            readout_j1_batch = readout.eval(feed_dict = {s : s_j1_batch})
-            print(readout_j1_batch)
-            for i in range(0, len(minibatch)):
-                terminal = minibatch[i][4]
-                # if terminal, only equals reward
-                if terminal:
-                    y_batch.append(r_batch[i])
-                else:
-                    y_batch.append(r_batch[i] + 0.99 * np.max(readout_j1_batch[i]))
-            #print(y_batch)
-            #print(len(s_j_batch))
-            #print(len(a_batch))
-            #print(len(r_batch))
-            #print(len(s_j1_batch))
-            train_step.run(feed_dict = {y : np.reshape(y_batch,(10,1)),a : np.reshape(a_batch,(10,2)),s : np.reshape(s_j_batch,(10,4))})
-
+   pass
+   
 
 
