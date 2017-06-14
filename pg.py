55b04bd9154d047d0f8fb3b85f51dd3d1e36c339
Li SH <li012589li@outlook.com>
2017-06-14 13:13:50 +0800

    add record & del unnessary code

--- pg.py
+++ pg.py
@@ -7,24 +7,11 @@ def reset_graph(seed=42):
     tf.set_random_seed(seed)
     np.random.seed(seed)
 
-import matplotlib
-import matplotlib.animation as animation
-import matplotlib.pyplot as plt
-plt.rcParams['axes.labelsize'] = 14
-plt.rcParams['xtick.labelsize'] = 12
-plt.rcParams['ytick.labelsize'] = 12
-
 PROJECT_ROOT_DIR = "."
 CHAPTER_ID = "rl"
 
-def save_fig(fig_id, tight_layout=True):
-    path = os.path.join(PROJECT_ROOT_DIR, "images", CHAPTER_ID, fig_id + ".png")
-    print("Saving figure", fig_id)
-    if tight_layout:
-        plt.tight_layout()
-    plt.savefig(path, format='png', dpi=300)
-
 import gym
+from gym import wrappers
 
 import tensorflow as tf
 
@@ -77,63 +64,8 @@ def discount_and_normalize_rewards(all_rewards, discount_rate):
     reward_std = flat_rewards.std()
     return [(discounted_rewards - reward_mean)/reward_std for discounted_rewards in all_discounted_rewards]
 
-from PIL import Image, ImageDraw
-
-openai_cart_pole_rendering = True   # no problem, let's use OpenAI gym's rendering function
-
-def plot_animation(frames, repeat=False, interval=40):
-    plt.close()  # or else nbagg sometimes plots in the previous cell
-    fig = plt.figure()
-    patch = plt.imshow(frames[0])
-    plt.axis('off')
-    return animation.FuncAnimation(fig, update_scene, fargs=(frames, patch), frames=len(frames), repeat=repeat, interval=interval)
-
-def render_cart_pole(env, obs):
-    if openai_cart_pole_rendering:
-        # use OpenAI gym's rendering function
-        return env.render(mode="rgb_array")
-    else:
-        # rendering for the cart pole environment (in case OpenAI gym can't do it)
-        img_w = 600
-        img_h = 400
-        cart_w = img_w // 12
-        cart_h = img_h // 15
-        pole_len = img_h // 3.5
-        pole_w = img_w // 80 + 1
-        x_width = 2
-        max_ang = 0.2
-        bg_col = (255, 255, 255)
-        cart_col = 0x000000 # Blue Green Red
-        pole_col = 0x669acc # Blue Green Red
-
-        pos, vel, ang, ang_vel = obs
-        img = Image.new('RGB', (img_w, img_h), bg_col)
-        draw = ImageDraw.Draw(img)
-        cart_x = pos * img_w // x_width + img_w // x_width
-        cart_y = img_h * 95 // 100
-        top_pole_x = cart_x + pole_len * np.sin(ang)
-        top_pole_y = cart_y - cart_h // 2 - pole_len * np.cos(ang)
-        draw.line((0, cart_y, img_w, cart_y), fill=0)
-        draw.rectangle((cart_x - cart_w // 2, cart_y - cart_h // 2, cart_x + cart_w // 2, cart_y + cart_h // 2), fill=cart_col) # draw cart
-        draw.line((cart_x, cart_y - cart_h // 2, top_pole_x, top_pole_y), fill=pole_col, width=pole_w) # draw pole
-        return np.array(img)
-def render_policy_net(model_path, action, X, n_max_steps = 1000):
-    frames = []
-    env = gym.make("CartPole-v0")
-    obs = env.reset()
-    with tf.Session() as sess:
-        saver.restore(sess, model_path)
-        for step in range(n_max_steps):
-            img = render_cart_pole(env, obs)
-            frames.append(img)
-            action_val = action.eval(feed_dict={X: obs.reshape(1, n_inputs)})
-            obs, reward, done, info = env.step(action_val[0][0])
-            if done:
-                break
-    env.close()
-    return frames
-
 env = gym.make("CartPole-v0")
+env = wrappers.Monitor(env, './cartpole')
 
 n_games_per_update = 10
 n_max_steps = 1000
