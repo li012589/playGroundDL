2e93b51deab66e2851c6ba9cd7c195c24a36e1fd
Li SH <li012589li@outlook.com>
2017-06-15 11:58:47 +0800

    convert to func

--- presentNN.py
+++ presentNN.py
@@ -11,6 +11,27 @@ TAU = 0.001
 STEP = [0.1,0.1,0.01,0.1]
 MAX_RANGE = 10
 
+def genPic(network,ranges,steps,choice,maxRange):
+    x_dot = []
+    theta_dot = []
+    one_r = []
+    zero_r = []
+    x = 0
+    theta = 0
+    ranges = [ranges[t] for t in choice]
+    for xDot in np.arange(ranges[0][0],ranges[0][1],STEP[0]):
+        for thetaDot in np.arange(ranges[1][0],ranges[1][1],STEP[1]):
+            x_dot.append(xDot)
+            theta_dot.append(thetaDot)
+            q = network.predict(np.reshape([x,xDot,theta,thetaDot],(1,network.sDim)))[0]
+            zero_r.append(q[0])
+            one_r.append(q[1])
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.plot_trisurf(x_dot, theta_dot, one_r,color = 'r')
+    ax.plot_trisurf(x_dot, theta_dot, zero_r,color = 'b')
+    plt.show()
+
 def main():
     env = gym.make(ENV_NAME)
     sDim = env.observation_space.shape[0]
@@ -23,7 +44,7 @@ def main():
         if high[i] >= MAX_RANGE:
             high[i] = MAX_RANGE
             low[i] = -MAX_RANGE
-        ranges.append([high[i],low[i]])
+        ranges.append([low[i],high[i]])
     #print(ranges)
     network = Qnetwork(sess,sDim,aDim,LEARNING_RATE,TAU)
     saver = tf.train.Saver()
@@ -33,31 +54,7 @@ def main():
         print("Successfully loaded:", checkpoint.model_checkpoint_path)
     else:
         print("Could not find old network weights")
-    x_dot = []
-    theta_dot = []
-    one_r = []
-    zero_r = []
-    x = 0
-    theta = 0
-    for xDot in np.arange(ranges[1][1],ranges[1][0],STEP[1]):
-        for thetaDot in np.arange(ranges[3][1],ranges[3][0],STEP[3]):
-            x_dot.append(xDot)
-            theta_dot.append(thetaDot)
-            q = network.predict(np.reshape([x,xDot,theta,thetaDot],(1,network.sDim)))[0]
-            zero_r.append(q[0])
-            one_r.append(q[1])
-    #print x_dot
-    #print theta_dot
-    #print zero_r
-    #print one_r
-    #print(len(x_dot))
-    fig = plt.figure()
-    ax = fig.add_subplot(111, projection='3d')
-    ax.plot_trisurf(x_dot, theta_dot, one_r,color = 'r')
-    #fig2 = plt.figure()
-    #ax2 = fig2.add_subplot(111, projection='3d')
-    ax.plot_trisurf(x_dot, theta_dot, zero_r,color = 'b')
-    #Axes3D.plot_wireframe(x_dot, theta_dot, zero_r, rstride=10, cstride=10)
-    plt.show()
+    steps = [STEP[t] for t in [1,3]]
+    genPic(network,ranges,steps,[1,3],MAX_RANGE)
 if __name__ == "__main__":
     main()
