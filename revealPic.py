672713eed3bcf2b38e5487bb0da7e8f7f29869b4
Li SH <li012589li@outlook.com>
2017-06-16 10:30:21 +0800

    add animation func

--- revealPic.py
+++ revealPic.py
@@ -5,4 +5,12 @@ from presentNN import genPic,save2Pic
 
 BASE_DIR_PIC = './picDir/'
 
-save2Pic(BASE_DIR_PIC,190,10)
+#save2Pic(BASE_DIR_PIC,67000,1000)
+
+import imageio
+images = []
+for i in range(0,67000,1000):
+    if i == 0 :
+        continue
+    images.append(imageio.imread(BASE_DIR_PIC+str(i)+'.png'))
+imageio.mimsave(BASE_DIR_PIC+'combine.gif', images)
