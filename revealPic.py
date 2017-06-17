7f8bce8d0dbe7fe78bc645773bd59ffa2651feed
Li SH <li012589li@outlook.com>
2017-06-17 13:30:48 +0800

    remove animation part

--- revealPic.py
+++ revealPic.py
@@ -5,12 +5,4 @@ from presentNN import genPic,save2Pic
 
 BASE_DIR_PIC = './picDir/'
 
-#save2Pic(BASE_DIR_PIC,67000,1000)
-
-import imageio
-images = []
-for i in range(0,67000,1000):
-    if i == 0 :
-        continue
-    images.append(imageio.imread(BASE_DIR_PIC+str(i)+'.png'))
-imageio.mimsave(BASE_DIR_PIC+'combine.gif', images)
+save2Pic(BASE_DIR_PIC,67000,1000)
