7a5819b37cc4fa50d9710261d83e6ee4b3e52a81
Li SH <li012589li@outlook.com>
2017-06-19 18:10:40 +0800

    add more theta

--- revealPic.py
+++ revealPic.py
@@ -2,10 +2,14 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import pickle
 from presentNN import genPic,save2Pic
+import math
 
 BASE_DIR_PIC = './picDir/'
-BASE = 591000
-MAX = 730000
+BASE = 0
+MAX = 6000
 STEP = 1000
+THETA1 = 0
+THETA2 = 10*2*math.pi/360
 
-save2Pic(BASE_DIR_PIC,BASE,MAX,STEP)
+save2Pic(BASE_DIR_PIC,BASE,MAX,STEP,THETA1)
+save2Pic(BASE_DIR_PIC,BASE,MAX,STEP,THETA2)
