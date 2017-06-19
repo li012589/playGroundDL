b4d96c8491a19d3571167ed2748a1eb79597e8c5
Li SH <li012589li@outlook.com>
2017-06-19 18:10:52 +0800

    add more thheta

--- animation.py
+++ animation.py
@@ -2,17 +2,25 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import pickle
 from presentNN import genPic,save2Pic
+import math
 
-BASE_DIR_PIC = './picDirPG/'
-PASS0 = False
-MAX = 300
-STEP = 1
+BASE_DIR_PIC = './picDir/'
+PASS0 = True
+MAX = 6000
+STEP = 1000
+THETA1 = 0
+THETA2 = 10*2*math.pi/360
 #save2Pic(BASE_DIR_PIC,67000,1000)
 
 import imageio
-images = []
-for i in range(0,MAX,STEP):
-    if i == 0 and PASS0:
-        continue
-    images.append(imageio.imread(BASE_DIR_PIC+str(i)+'.png'))
-imageio.mimsave(BASE_DIR_PIC+'combine.gif', images)
+def saveAnimation(BASE_DIR,maxstep,step,theta,pass0):
+    images = []
+    for i in range(0,maxstep,step):
+        if i == 0 and pass0:
+            continue
+        images.append(imageio.imread(BASE_DIR+str(i)+"theta="+str(theta)+'.png'))
+    imageio.mimsave(BASE_DIR+'combine_theta'+str(theta)+'.gif', images)
+
+if __name__ == '__main__':
+    saveAnimation(BASE_DIR_PIC,MAX,STEP,THETA1,PASS0)
+    saveAnimation(BASE_DIR_PIC,MAX,STEP,THETA2,PASS0)
